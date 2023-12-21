393735533589880132bb1514277fd36d41940b0a
sigma83l <hamedsedaghatgit83@gmail.com>
2023-12-21 17:08:27 +0200

    main module updated
    app.module.ts file updated

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,11 @@ import { PrismaModule } from './modules/prisma/prisma.module';
 import { StudentModule } from './modules/student/student.module';
 import { CoordinatorModule } from './modules/coordinator/coordinator.module';
 import { SuperviserModule } from './modules/superviser/superviser.module';
+import { ApplicationModule } from './modules/application/application.module';
+import { CompanyModule } from './modules/company/company.module';
 
 @Module({
-  imports: [PrismaModule, StudentModule, CoordinatorModule, SuperviserModule],
+  imports: [PrismaModule, StudentModule, CoordinatorModule, SuperviserModule, ApplicationModule, CompanyModule],
   controllers: [],
   providers: [],
 })
